d0c2b815fb233f7c95e06851070ef73bf1e3abd5
tomascabrerizo <tomascabrerizo97@gmail.com>
2024-05-21 15:19:36 -0300

    refactor js code

--- wsym.js
+++ wsym.js
@@ -101,17 +101,6 @@ function update_element_display(element, content, display) {
     }
 }
 
-function arrow_was_click() {
-    if(fullscreen_video.style.display === "block") {
-        fullscreen_video.pause();
-    }
-
-    // NOTE: only close the modal if we have a full screen element
-    if(fullscreen_element.style.display === "block") {
-        fullscreen_element.style.display = "none"
-    }
-
-}
 
 //////////////////////////////////////////////////////////////
 // Audio functions
@@ -152,19 +141,6 @@ function register_new_color(name) {
     return color;
 }
 
-function update_sender_name_color(name_element, name) {
-
-
-    // NOTE: register name to always use the same color
-    let color = sender_name_map[name]
-
-    if(color === undefined) {
-        color = register_new_color(name)
-    }
-
-    name_element.style.color = color;
-}
-
 function update_quote_name_color(quote_name_element, quote_color, quote_name) {
 
     if(quote_name === null) return
@@ -179,33 +155,15 @@ function update_quote_name_color(quote_name_element, quote_color, quote_name) {
     quote_color.style.backgroundColor = color
 }
 
-function update_time_when_message_is_image_or_video_only(message) {
-    // NOTE: Update time when message have an image and not text
-    const message_image = message.getAttribute("image-content");
-    const message_video = message.getAttribute("video-content");
-   
-    let container = null;
-    if(message_image !== null) {
-        container = message.querySelector(".wsym-message-image-container");
-    } else if(message_video !== null) {
-        container = message.querySelector(".wsym-message-video-container");
-    } else {
-        return;
+
+function arrow_was_click() {
+    if(fullscreen_video.style.display === "block") {
+        fullscreen_video.pause();
     }
 
-    // NOTE: Modify position of the message time and check if the image has no message content
-    const text_content = message.getAttribute("text-content");
-    if(text_content !== null) {
-        const message_info = container.querySelector(".wsym-message-info-image");
-        message_info.style.display = "none";
-    } else {
-        const message_info = message.querySelector(".wsym-message-info");
-        message_info.style.display = "none";
-
-        // NOTE: Update Image info content
-        const time = message.getAttribute("time");
-        const time_element = container.querySelector(".wsym-message-info-image").querySelector(".wsym-message-time");
-        time_element.innerHTML = time;
+    // NOTE: only close the modal if we have a full screen element
+    if(fullscreen_element.style.display === "block") {
+        fullscreen_element.style.display = "none"
     }
 }
 
@@ -228,29 +186,19 @@ function generate_header(app_container_element) {
     header_status_element.innerHTML = "<p>"+app_container_element.getAttribute("status")+"</p>";
 }
 
-let image_count = 1;
-
 function set_video_poster(poster_element, video) {
-
     video.addEventListener('loadedmetadata', function(event) {
-        
         video.currentTime = 0.1;
-        
         video.addEventListener('seeked', function() {
-            
             if(video.generate_thumbnail == true) {
-                
                 var canvas = document.createElement('canvas');
                 canvas.width = video.videoWidth;
                 canvas.height = video.videoHeight;
                 var context = canvas.getContext('2d');
                 context.drawImage(video, 0, 0, canvas.width, canvas.height);
-                
                 var dataURL = canvas.toDataURL();
                 poster_element.setAttribute('src', dataURL);
-                
                 video.currentTime = 0;
-                
                 video.generate_thumbnail = false;
             }
 
@@ -259,11 +207,61 @@ function set_video_poster(poster_element, video) {
     
 }
 
-function generate_mesage(template, message) {
-    // NOTE: Add template to message 
-    message.appendChild(template.content.cloneNode(true));
+function update_element_top_and_bottom_margin(element, message) {
+    // NOTE: Update element top margin
+    const forwarded = message.getAttribute("forwarded");
+    const sender_name = message.getAttribute("sender-name");
+    if(sender_name !== null || (forwarded === null || forwarded === false)) {
+        element.style.marginTop = 0;
+    }
 
-    // NOTE: Update message sender image
+    const text_content = message.getAttribute("text-content");
+    console.log(text_content);
+    if(text_content !== null) {
+       element.style.marginBottom = 0;
+    }
+    
+}
+
+function update_element_display(element, content, display) {
+    if(element === null) return;
+
+    if(content === null || content === "") {
+        element.style.display = "none";
+        return false;
+    } else {
+        element.style.display = display;
+        return true;
+    }
+}
+
+function update_quote(message) {
+    const quote_content = message.getAttribute("quote-content");
+    const quote_element = message.querySelector(".wsym-message-quote");
+
+    if(update_element_display(quote_element, quote_content, "flex")) {
+        const quote_content_element = message.querySelector(".wsym-message-quote-content");
+        quote_content_element.innerHTML = quote_content;
+    
+        const quote_name = message.getAttribute("quote-name");
+        const quote_name_element = message.querySelector(".wsym-message-quote-name");
+        quote_name_element.innerHTML = quote_name;
+        const quote_color = message.querySelector(".wsym-message-quote-color")
+        update_quote_name_color(quote_name_element, quote_color, quote_name);
+        
+        const quote_image = message.getAttribute("quote-image");
+        const quote_image_element = message.querySelector(".wsym-message-quote-image");
+        if(quote_image !== null) {
+            quote_image_element.setAttribute("src", quote_image);
+        }
+    
+    
+        update_element_top_and_bottom_margin(quote_element, message);
+    }
+
+}
+
+function update_sender_image(message) {
     const sender_image_content = message.getAttribute("sender-image");
     const sender_image_element = message.querySelector(".wsym-message-sender-image");
     if(update_element_display(sender_image_element, sender_image_content, "block")) {
@@ -275,14 +273,25 @@ function generate_mesage(template, message) {
             sender_image_element.style.visibility = "hidden";
         }
     }
+}
 
-    // NOTE: Update message content
+function update_text_content(message) {
     const text_content = message.getAttribute("text-content");
     const text_element = message.querySelector(".wsym-message-content");
     if(update_element_display(text_element, text_content, "block")) {
         text_element.innerHTML = "<p>"+text_content+"</p>";
     }
+}
 
+function update_sender_name_color(name_element, name) {
+    let color = sender_name_map[name]
+    if(color === undefined) {
+        color = register_new_color(name)
+    }
+    name_element.style.color = color;
+}
+
+function update_message_sender_name(message) {
     // NOTE: Update message name
     const sender_name_content = message.getAttribute("sender-name");
     const sender_name_element = message.querySelector(".wsym-message-name");
@@ -290,30 +299,47 @@ function generate_mesage(template, message) {
         sender_name_element.innerHTML = sender_name_content;
         update_sender_name_color(sender_name_element, sender_name_content);
     }
+}
 
-    // NOTE: Update message quote
-    const quote_content = message.getAttribute("quote-content");
-    const quote_content_element = message.querySelector(".wsym-message-quote-content");
-    quote_content_element.innerHTML = quote_content;
-    
-    const quote_name = message.getAttribute("quote-name");
-    const quote_name_element = message.querySelector(".wsym-message-quote-name");
-    quote_name_element.innerHTML = quote_name;
-    const quote_color = message.querySelector(".wsym-message-quote-color")
-    update_quote_name_color(quote_name_element, quote_color, quote_name);
-    
-    const quote_image = message.getAttribute("quote-image");
-    const quote_image_element = message.querySelector(".wsym-message-quote-image");
-    if(quote_image !== null) {
-        quote_image_element.setAttribute("src", quote_image);
+function update_time_when_message_is_image_or_video_only(message) {
+    // NOTE: Update time when message have an image and not text
+    const message_image = message.getAttribute("image-content");
+    const message_video = message.getAttribute("video-content");
+   
+    let container = null;
+    if(message_image !== null) {
+        container = message.querySelector(".wsym-message-image-container");
+    } else if(message_video !== null) {
+        container = message.querySelector(".wsym-message-video-container");
+    } else {
+        return;
     }
+    // NOTE: Modify position of the message time and check if the image has no message content
+    const text_content = message.getAttribute("text-content");
+    if(text_content !== null) {
+        const message_info = container.querySelector(".wsym-message-info-image");
+        message_info.style.display = "none";
+    } else {
+        const message_info = message.querySelector(".wsym-message-info");
+        message_info.style.display = "none";
 
-    // NOTE: Update message time
+        // NOTE: Update Image info content
+        const time = message.getAttribute("time");
+        const time_element = container.querySelector(".wsym-message-info-image").querySelector(".wsym-message-time");
+        time_element.innerHTML = time;
+    }
+}
+
+function update_time(message) {
     const time = message.getAttribute("time");
     const time_element = message.querySelector(".wsym-message-info").querySelector(".wsym-message-time");
     time_element.innerHTML = time;
+    update_time_when_message_is_image_or_video_only(message);
 
-    // NOTE: Update Image content
+}
+
+function update_image(message) {
+
     const image_content = message.getAttribute("image-content");
     const image_container_element = message.querySelector(".wsym-message-image-container");
     const image_element = message.querySelector(".wsym-message-image");
@@ -322,9 +348,14 @@ function generate_mesage(template, message) {
         image_element.onclick = function() {
             element_request_fullscreen(image_element, message);
         }
+        update_element_top_and_bottom_margin(image_container_element, message);
     }
 
-    // NOTE: Update Video content
+    
+}
+
+function update_video(message) {
+
     const video_content = message.getAttribute("video-content");
     const video_container_element = message.querySelector(".wsym-message-video-container");
     const poster_element = message.querySelector(".wsym-message-video");
@@ -333,6 +364,8 @@ function generate_mesage(template, message) {
         const video_element = document.createElement("video");
         video_element.generate_thumbnail = true;
         video_element.setAttribute("preload", "metadata");
+        video_element.setAttribute("playsinline", "playsinline");
+        video_element.setAttribute("webkit-playsinline", "webkit-playsinline");
         video_element.setAttribute("src", video_content);
         
         set_video_poster(poster_element, video_element);
@@ -350,10 +383,13 @@ function generate_mesage(template, message) {
                 fullscreen_video.play()
             }
         }
-
+        
+        update_element_top_and_bottom_margin(video_container_element, message);
     }
+}
 
-    // NOTE: Update Audio content
+function update_audio(message) {
+
     const audio_content = message.getAttribute("audio-content");
     const audio_container_element = message.querySelector(".wsym-message-audio-container");
     const audio_element = message.querySelector(".wsym-message-audio");
@@ -407,14 +443,29 @@ function generate_mesage(template, message) {
         });
 
     }
+}
 
-    update_time_when_message_is_image_or_video_only(message);
-
+function update_sticker(message) {
     const sticker_content = message.getAttribute("sticker-content");
     const sticker_element = message.querySelector(".wsym-message-sticker");
     if(update_element_display(sticker_element, sticker_content)) {
         sticker_element.setAttribute("src", sticker_content);
     }
+}
+
+function generate_mesage(template, message) {
+    // NOTE: Add template to message 
+    message.appendChild(template.content.cloneNode(true));
+
+    update_sender_image(message);
+    update_text_content(message);
+    update_message_sender_name(message);
+    update_quote(message);
+    update_image(message);
+    update_video(message);
+    update_audio(message);
+    update_sticker(message);
+    update_time(message);
 }
 
 // NOTE App container element
